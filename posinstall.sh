d1d6f69c51700623e2fed4ea669e4d143480fdb4
gunnar <vrms@netcologne.de>
2017-04-05 07:53:28 +0800

    Create posinstall.sh
    just a first draft, to be amended to I guess

---
+++ posinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this is my personal postinstall script for deb/ubuntu that installs some of the packages I like to have on any server
+# it's totally subjective and usefull for me but may seem stupod to others. 
+# If so, just move on or send me a PR if you want to suggest something helpfull
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install vim \
+                     htop \
+                     btrfs-tools \
+                     openssh-server
+
+                   
+                     
